4d75a529c1f8e82768f6f7152feaf81ff83e6f92
Mayank Jain <mayank13@gmail.com>
2015-10-26 12:24:28 +0530

    Pushing changes for Goal Selection Screen
    Integrating the Horizontal List View Changes

--- app/src/main/java/com/cititmobilechallenge/citifit/LoginActivity.java
+++ app/src/main/java/com/cititmobilechallenge/citifit/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.cititmobilechallenge.citifit;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.cititmobilechallenge.citifit.common.FontHelper;
 
@@ -10,12 +13,23 @@ import com.cititmobilechallenge.citifit.common.FontHelper;
  */
 public class LoginActivity extends Activity {
 
+    Button btnLogin = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        btnLogin =(Button)findViewById(R.id.btnLogin);
 
         FontHelper.applyFont(this, findViewById(R.id.rl_login_container));
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent i = new Intent(LoginActivity.this,RewardActivity.class);
+                startActivity(i);
+            }
+        });
     }
 
 }
