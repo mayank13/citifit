5f35b09db416bea1ea58044bbea1d0a17aaa7f25
ashwini009 <kumar.ashwini009@gmail.com>
2015-10-30 01:17:54 +0530

    Full screen enabled for Login and Splash Screens

--- app/src/main/java/com/cititmobilechallenge/citifit/activity/LoginActivity.java
+++ app/src/main/java/com/cititmobilechallenge/citifit/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.cititmobilechallenge.citifit.R;
@@ -19,6 +20,8 @@ public class LoginActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         btnLogin =(Button)findViewById(R.id.btnLogin);
 
