bca89fc6224cd868e892be293b299373e2ea7a20
ashwini009 <kumar.ashwini009@gmail.com>
2015-10-29 22:17:51 +0530

    Profile Fragment implemented

--- app/src/main/java/com/cititmobilechallenge/citifit/fragments/ProfileFragment.java
+++ app/src/main/java/com/cititmobilechallenge/citifit/fragments/ProfileFragment.java
@@ -1,15 +1,22 @@
 package com.cititmobilechallenge.citifit.fragments;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.cititmobilechallenge.citifit.R;
+import com.cititmobilechallenge.citifit.common.FontHelper;
+import com.cititmobilechallenge.citifit.common.ImageHelper;
 
 
 public class ProfileFragment extends Fragment {
+
+    private ImageView mIvProfile = null;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -24,7 +31,18 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        FontHelper.applyFont(getActivity(), view.findViewById(R.id.rl_profie_container));
+
+        mIvProfile = (ImageView) view.findViewById(R.id.profileImage);
+
+        Bitmap profileImage = BitmapFactory.decodeResource(getResources(), R.drawable.profile);
+        Bitmap circularProfileImage = ImageHelper.getRoundedCornerBitmap(profileImage, 200);
+
+        mIvProfile.setImageBitmap(circularProfileImage);
+
+        return view;
     }
 
 }
