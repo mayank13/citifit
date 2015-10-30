43eb2859f295e50e71f4d5a91d37b9f156393e32
ashwini009 <kumar.ashwini009@gmail.com>
2015-10-31 00:25:47 +0530

    Parse Channel Name Change

--- app/src/main/java/com/cititmobilechallenge/citifit/application/AppConfig.java
+++ app/src/main/java/com/cititmobilechallenge/citifit/application/AppConfig.java
@@ -4,7 +4,7 @@ package com.cititmobilechallenge.citifit.application;
  * Created by Mayank Jain
  */
 public class AppConfig {
-    public static final String PARSE_CHANNEL = "AndroidHive";
+    public static final String PARSE_CHANNEL = "CitiFit";
     public static final String PARSE_APPLICATION_ID = "3kVzn5ywKHuEzmrmhrbpAVZK7Qj39qHbaermdroW";
     public static final String PARSE_CLIENT_KEY = "ngrluVLztOHAXHeqhcFJwJeiiRltpUuMxtB4KJat";
     public static final int NOTIFICATION_ID = 100;
